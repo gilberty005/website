f8934aaa78aaf28da9775afd3f3e76d9b5540c7a
Gilbert Yang <gilberty005@gmail.com>
2024-01-03 15:11:16 -0800

    v1

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri'; 
+import logo from '../../assets/logo.svg';
 import './navbar.css'; 
 
 const Navbar = () => {
     return (
-        <div>
-            Navbar
+        <div className="gpt3_navbar"> 
+            <div className = "gtp3_navbar-links">
+                <div className = "gpt3_navbar-links_logo">
+                    <img src={logo} alt="logo"/>
+                </div>
+            </div>
         </div>
     )
 }
