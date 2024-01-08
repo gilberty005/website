a5e7f0617cb92b361cad078ee0edb56dc4c150d5
Gilbert Yang <gilberty005@gmail.com>
2024-01-07 22:07:59 -0800

    added portfolio page draft

--- src/pages/portfoliofolder/portfolio.js
+++ src/pages/portfoliofolder/portfolio.js
@@ -1,27 +1,78 @@
 import React from "react";
-import { motion } from 'framer-motion';
-
-import { styles } from '../../styles';
+import {
+    VerticalTimeline,
+    VerticalTimelineElement,
+  } from "react-vertical-timeline-component";
+import { motion } from "framer-motion";
 import { ComputersCanvas } from '../../components/canvas';
+import { experiences } from "../../constants";
+import "react-vertical-timeline-component/style.min.css";
 import './portfolio.css';
+import { textVariant } from "../../utils/motion";
  
+const ExperienceCard = ({ experience }) => {
+    return (
+      <VerticalTimelineElement
+        contentStyle={{
+          background: "#1d1836",
+          color: "#fff",
+        }}
+        contentArrowStyle={{ borderRight: "7px solid  #232631" }}
+        date={experience.date}
+        iconStyle={{ background: experience.iconBg }}
+        icon={
+          <div className='flex justify-center items-center w-full h-full'>
+            <img
+              src={experience.icon}
+              alt={experience.company_name}
+              className='w-[60%] h-[60%] object-contain'
+            />
+          </div>
+        }
+      >
+        <div>
+          <h3 className='text-white text-[24px] font-bold'>{experience.title}</h3>
+          <p
+            className='text-secondary text-[16px] font-semibold'
+            style={{ margin: 0 }}
+          >
+            {experience.company_name}
+          </p>
+        </div>
+  
+        <ul className='mt-5 list-disc ml-5 space-y-2'>
+          {experience.points.map((point, index) => (
+            <li
+              key={`experience-point-${index}`}
+              className='text-white-100 text-[14px] pl-1 tracking-wider'
+            >
+              {point}
+            </li>
+          ))}
+        </ul>
+      </VerticalTimelineElement>
+    );
+};
+
 export function Portfolio() {
     return (
         <>
-            <section className="relative w-full h-screen mx-auto hero">
-                <div className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
-                    <div className='flex flex-col justify-center items-center mt-5'>
-                        <div className='w-5 h-5 rounded-full bg-[#915EFF]' />
-                        <div className='w-1 sm:h-80 h-40 violet-gradient' />
-                    </div>
+            <section>
+                <motion.div variants={textVariant()}>
+                <h2 className={`gradient__text text-center`}>
+                    Timeline of my Experiences
+                </h2>
+                </motion.div>
 
-                    <div >
-                        <h1 className="text-4xl font-bold gradient__text">Welcome to my Portfolio</h1>
-                        <p className="mt-2 text-white-100">
-                            I'm currently a freshman at Columbia University majoring in <br className='sm:block hidden' />
-                            computer science and economics.
-                        </p>
-                    </div>
+                <div className='mt-20 flex flex-col'>
+                    <VerticalTimeline>
+                        {experiences.map((experience, index) => (
+                            <ExperienceCard
+                            key={`experience-${index}`}
+                            experience={experience}
+                            />
+                        ))}
+                    </VerticalTimeline>
                 </div>
             </section>
         </>
