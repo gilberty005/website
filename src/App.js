5bf2223499f691e52a0aef029668843239850031
Gilbert Yang <gilberty005@gmail.com>
2024-01-12 16:23:40 -0800

    added auth

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
-import {Blog} from './pages/blogfolder/blog';
-import {Contact} from './pages/contactfolder/contact';
-import {Portfolio} from './pages/portfoliofolder/portfolio';
-import {Projects} from './pages/projectsfolder/projects';
-import {Home} from './pages/homefolder/home';
-import {Notfound} from './pages/notfoundfolder/notfound'; 
-import { Footer} from './containers'; 
-import { Navbar } from './components'; 
+import { Blog } from './pages/blogfolder/blog';
+import { Contact } from './pages/contactfolder/contact';
+import { Portfolio } from './pages/portfoliofolder/portfolio';
+import { Projects } from './pages/projectsfolder/projects';
+import { Home } from './pages/homefolder/home';
+import { Notfound } from './pages/notfoundfolder/notfound';
+import { Footer } from './containers';
+import { Navbar } from './components';
+import "@aws-amplify/ui-react/styles.css";
+import { withAuthenticator, Button, Heading, View, Card } from "@aws-amplify/ui-react";
 
-const App = () => {
+const App = ({ signOut }) => {
   return (
     <Router>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/portfolio" element={<Portfolio/>}/>
-        <Route path="/projects" element={<Projects/>}/>
-        <Route path="/blog" element={<Blog/>}/>
-        <Route path="/contact" element={<Contact/>}/>
-        <Route path="*" element={<Notfound/>}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/blog" element={<Blog />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Notfound />} />
       </Routes>
       <Footer />
+      <View className="App">
+        <Card>
+          <Heading level={1}>We now have Auth!</Heading>
+        </Card>
+        <Button onClick={signOut}>Sign Out</Button>
+      </View>
     </Router>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
